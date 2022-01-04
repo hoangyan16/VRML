c624fdeb2e6b735b39d2f4553f86c7d80f918d29
hoangyan16@gmail.com <hoangyan16@gmail.com>
2022-01-04 22:36:39 +0700

    first commit

---
+++ Scenes/ChangeScale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Change()
+    {
+        transform.localScale= transform.localScale + new Vector3(1f,1f,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scenes/sceneManagement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void WaitMuseum()
+    {
+        Invoke("Museum",2);
+
+    }
+
+    // Update is called once per frame
+    public void Museum()
+    {
+        SceneManager.LoadScene("1");
+    }
+
+     public void WaitMall()
+    {
+        Invoke("Mall",2);
+    }
+
+    // Update is called once per frame
+    public void Mall()
+    {
+        SceneManager.LoadScene("2");
+    }
+
+     public void WaitHome()
+    {
+        Invoke("Home",2);
+    }
+
+    // Update is called once per frame
+    public void Home()
+    {
+        SceneManager.LoadScene("1");
+    }
+
+         public void WaitPark()
+    {
+        Invoke("Park",2);
+    }
+
+    // Update is called once per frame
+    public void Park()
+    {
+        SceneManager.LoadScene("1");
+    }
+         public void WaitCompany()
+    {
+        Invoke("Company",2);
+    }
+
+    // Update is called once per frame
+    public void Company()
+    {
+        SceneManager.LoadScene("1");
+    }
+         public void WaitFactory()
+    {
+        Invoke("Factory",2);
+    }
+
+    // Update is called once per frame
+    public void Factory()
+    {
+        SceneManager.LoadScene("1");
+    }
+}
